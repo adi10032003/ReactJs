1bc9d997ebe1f4388cc3811a6eda2ac800f6aa7b
adi10032003 <adityasingh99048@gmail.com>
2026-05-10 02:21:45 +0530

    Completed counter

--- counter_reactjs/src/Counter/Counter.jsx
+++ counter_reactjs/src/Counter/Counter.jsx
@@ -4,15 +4,15 @@ const Counter = () => {
     const[count,countvalue]=useState(0)
   return (
     <div className='Main-Container'>
-      <counter className="container">
+      <div className="container">
         <h1>Counter</h1>
-        <h1>You have clicked it {count} times</h1>
+        <div className='heading'>You have clicked it {count} times</div>
         <div className='counterbutton'>
             <button onClick={()=>{countvalue(count+1)}} className='increment button'>Increase</button>
             <button onClick={()=>{countvalue(0)}} className='reset'>Reset</button>
 
         </div>
-      </counter>
+      </div>
     </div>
   )
 }
